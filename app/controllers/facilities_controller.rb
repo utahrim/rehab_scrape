19c4b9db1522a0b64285ae2f5589e804eec27d76
InmateAid4 <utahrim@gmail.com>
2016-07-19 14:19:57 -0400

    Decrease time of sleep on error, puts nil as telephone if blank

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -13,11 +13,12 @@ class FacilitiesController < ApplicationController
 
   def search
     @driver = Selenium::WebDriver.for :chrome
-    states_array = ["ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy", "al", "ak", "az", "ar", "ca", "co", "ct", "de", "dc", "fl", "ga", "hi", "id", "il", "in"]
+    states_array = ["al", "ak", "az", "ar", "ca", "co", "ct", "de", "dc", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy"]
     states_array.each do |state_site|
       @driver.get ("http://www.countyoffice.org/#{state_site}-police-department/")
       @wait = Selenium::WebDriver::Wait.new(:timeout => 20)
       @p = 1
+      @l = 0
       begin
         page_array = @wait.until {@driver.find_elements(:class, "mob-clip")}
         info_counter = page_array.count - 1
@@ -35,7 +36,7 @@ class FacilitiesController < ApplicationController
         retry
       rescue Net::ReadTimeout
         puts "Net::ReadTimeout"
-        sleep(15.minutes)
+        sleep(5.minutes)
         rescue_error(state_site)
         retry
       rescue Selenium::WebDriver::Error::UnknownError
@@ -50,7 +51,6 @@ class FacilitiesController < ApplicationController
   def page_loop(state_site, info_counter)
     pages = @wait.until {@driver.find_elements(:xpath, "//li").last.text.to_i}
     until @p > pages do
-      @l = 0
       click(info_counter)
       @p += 1
       @driver.get ("http://www.countyoffice.org/#{state_site}-police-department-p#{@p}/")
@@ -75,7 +75,7 @@ class FacilitiesController < ApplicationController
     city = @driver.find_elements(:class, "addressLocality").first.text
     state = @driver.find_elements(:class, "addressRegion").first.text
     address = @driver.find_elements(:class, "address").first.text
-    phone = @driver.find_elements(:class, "telephone").first.text
+    phone = @driver.find_elements(:class, "telephone").blank? ? nil : @driver.find_elements(:class, "telephone").first.text
     create_facility(name, city, state, address, phone)
   end
 
