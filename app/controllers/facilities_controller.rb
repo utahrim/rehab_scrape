5e889e872dfb7b0c1b63a57e758617851a6e9ec2
InmateAid4 <utahrim@gmail.com>
2016-07-20 14:39:53 -0400

    crawl all categories

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -13,37 +13,43 @@ class FacilitiesController < ApplicationController
 
   def search
     @driver = Selenium::WebDriver.for :chrome
-    states_array = ["de", "dc", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy", "al", "ak", "az", "ar", "ca", "co", "ct"]
-    states_array.each do |state_site|
-      @driver.get ("http://www.countyoffice.org/#{state_site}-elections/")
-      @wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-      @p = 1
-      @l = 0
-      begin
-        page_array = @wait.until {@driver.find_elements(:class, "mob-clip")}
-        info_counter = page_array.count - 1
-        page_loop(state_site, info_counter)
 
-      rescue Selenium::WebDriver::Error::StaleElementReferenceError
-        puts "Selenium::WebDriver::Error::StaleElementReferenceError"
-        sleep(1)
-        rescue_error(state_site)
-        retry
-      rescue NoMethodError
-        puts "NoMethodError"
-        sleep(1)
-        rescue_error(state_site)
-        retry
-      rescue Net::ReadTimeout
-        puts "Net::ReadTimeout"
-        sleep(5.minutes)
-        rescue_error(state_site)
-        retry
-      rescue Selenium::WebDriver::Error::UnknownError
-        puts "Selenium::WebDriver::Error::UnknownError"
-        sleep(1)
-        rescue_error(state_site)
-        retry
+    url_array = ["child-support-offices", "clerk", "colleges", "madical-corner", "courts", "motor-vehicles-dmv", "district-attorney", "fire-departments", "hospitals", "irs-offices", "jails-prisons", "libraries", "parks-department", "police-department", "post-office", "private-schools", "probation-department", "public-schools", "recorder-of-deeds", "sheriff", "social-security-office", "treasurer-tax-collector", "unemploymeny-offices", "assessor", "elections", "chamber-of-commerce"]
+
+    url_array.each do |cat|
+      states_array = ["al", "ak", "az", "ar", "ca", "co", "ct", "de", "dc", "fl", "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md", "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm", "ny", "nc", "nd", "oh", "ok", "or", "pa", "ri", "sc", "sd", "tn", "tx", "ut", "vt", "va", "wa", "wv", "wi", "wy"]
+
+      states_array.each do |state_site|
+        @driver.get ("http://www.countyoffice.org/#{state_site}-#{cat}/")
+        @wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+        @p = 1
+        @l = 0
+        begin
+          page_array = @wait.until {@driver.find_elements(:class, "mob-clip")}
+          info_counter = page_array.count - 1
+          page_loop(state_site, info_counter)
+
+        rescue Selenium::WebDriver::Error::StaleElementReferenceError
+          puts "Selenium::WebDriver::Error::StaleElementReferenceError"
+          sleep(1)
+          rescue_error(state_site)
+          retry
+        rescue NoMethodError
+          puts "NoMethodError"
+          sleep(1)
+          rescue_error(state_site)
+          retry
+        rescue Net::ReadTimeout
+          puts "Net::ReadTimeout"
+          sleep(5.minutes)
+          rescue_error(state_site)
+          retry
+        rescue Selenium::WebDriver::Error::UnknownError
+          puts "Selenium::WebDriver::Error::UnknownError"
+          sleep(1)
+          rescue_error(state_site)
+          retry
+        end
       end
     end
   end
@@ -53,7 +59,7 @@ class FacilitiesController < ApplicationController
     until @p > pages do
       click(info_counter)
       @p += 1
-      @driver.get ("http://www.countyoffice.org/#{state_site}-elections-p#{@p}/")
+      @driver.get ("http://www.countyoffice.org/#{state_site}-#{cat}-p#{@p}/")
     end
   end
 
@@ -85,10 +91,8 @@ class FacilitiesController < ApplicationController
   end
 
   def rescue_error(state_site)
-    @driver.get ("http://www.countyoffice.org/#{state_site}-elections-p#{@p}/")
+    @driver.get ("http://www.countyoffice.org/#{state_site}-#{cat}-p#{@p}/")
     @wait = Selenium::WebDriver::Wait.new(:timeout => 20)
     @page_array =  @wait.until { @driver.find_elements(:class, "mob-clip") }
   end
 end
-
-# url_array = ["assesor", "elections", "chamber-of-commerce", "child-support-offices", "clerk", "colleges", "madical-corner", "courts", "motor-vehicles-dmv", "district-attorney", "fire-departments", "hospitals", "irs-offices", "jails-prisons" ]
